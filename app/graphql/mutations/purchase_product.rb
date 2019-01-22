e48898593e8e83cdf913ddffb90e16a253ff6ad0
Simon Krol <simonkrol11@gmail.com>
2019-01-22 06:10:08 -0500

    seed database and fail if invalid product

--- app/graphql/mutations/purchase_product.rb
+++ app/graphql/mutations/purchase_product.rb
@@ -7,9 +7,12 @@ module Mutations
     argument :id, Integer, required: true
 
     def resolve(**args)
+      # product = Product.where(id: args[:id])
+      if(!Product.exists?(args[:id]))
+        return
+      end
       product = Product.find(args[:id])
 
-      return unless product
       if(product[:inventory_count] > 0)
         product[:inventory_count] = product[:inventory_count] - 1
         product.save
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create(title: 'Product 1', price: 4.99, inventory_count: 25)
+Product.create(title: 'Product 2', price: 8.99, inventory_count: 15)
+Product.create(title: 'Product 3', price: 44.99, inventory_count: 1)
+Product.create(title: 'Product 4', price: 14.99, inventory_count: 0)
+Product.create(title: 'Product 5', price: 3.99, inventory_count: 0)
+Product.create(title: 'Product 6', price: 23.87, inventory_count: 5)
+Product.create(title: 'Product 7', price: 102.99, inventory_count: 1)
+Product.create(title: 'Product 8', price: 77.99, inventory_count: 0)
+Product.create(title: 'Product 9', price: 0.99, inventory_count: 12)
+Product.create(title: 'Product 10', price: 1.99, inventory_count: 86)
